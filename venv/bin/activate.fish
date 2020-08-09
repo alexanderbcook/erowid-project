1e849d3c1a2d8ed782538ce919823bfd5961f969
Alex Cook <alex.chef.cook@gmail.com>
2020-08-09 09:03:15 -0700

    reset venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alexandercook/Repositories/erowid/venv"
+set -gx VIRTUAL_ENV "/Users/alexandercook/Repositories/erowid-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
