afde142ca276c6e292be63386e5060cfb4bf842b
Alex Cook <alex.chef.cook@gmail.com>
2020-08-10 09:43:03 -0700

    final commit to major rework of the entire project. i have the data in database!

--- erowid-api/fetch-experiences.py
+++ erowid-api/fetch-experiences.py
@@ -29,7 +29,6 @@ cursor.execute("SELECT id, name FROM erowid.substances ORDER BY id ASC;")
 for value in cursor.fetchall():
     substanceId = value[0]
     substanceName = value[1]
-
     experienceListXmlComplete = requests.get('https://erowid.org/experiences/research/exp_api.php?api_code='+api_key+'&a=experience_list&substance_id='+str(substanceId)+'&format=xml', headers=headers)
     experienceListXmlSplit = experienceListXmlComplete.text.split('</request-parameters>')[1]
     root = ET.fromstring(experienceListXmlSplit)
@@ -40,28 +39,29 @@ for value in cursor.fetchall():
 
         #Iterate through each experience. Check if it has already been processesed. 
         for experienceId in experienceArray:
+
+            experienceRequest = requests.get('https://erowid.org/experiences/research/exp_api.php?api_code='+api_key+'&a=experience_data&experience_id='+experienceId+'&format=xml', headers=headers)
             time.sleep(.15)
-            if not str(experienceId) in experienceAlreadyCaptured:
+            print('Fetched '+substanceName+' experience from URL: https://erowid.org/experiences/research/exp_api.php?api_code='+api_key+'&a=experience_data&experience_id='+experienceId+'&format=xml')
 
-                experienceRequest = requests.get('https://erowid.org/experiences/research/exp_api.php?api_code='+api_key+'&a=experience_data&experience_id='+experienceId+'&format=xml', headers=headers)
-                print('Fetched '+substanceName+' experience from URL: https://erowid.org/experiences/research/exp_api.php?api_code='+api_key+'&a=experience_data&experience_id='+experienceId+'&format=xml')
-
-                #Parse and upload data into database.
-                experienceXmlData = experienceRequest.text.split('</request-parameters>')[1]
+            #Parse and upload data into database. 
+            experienceXmlData = experienceRequest.text.split('</request-parameters>')[1]
+            try:
+                root = ET.fromstring(experienceXmlData.replace('&',''))
+            except:
+                pass
+            for child in root.findall('experience'):
                 try:
-                    root = ET.fromstring(experienceXmlData.replace('&',''))
+                    cursor.execute("INSERT INTO erowid.experiences (id, principal_substance, principal_substance_id, list_number, title, author, substance_string, body_weight, gender, published_date, submitted_date, experience_year, intensity, primary_category_id, substance_id_list, category_id_list, published_rating, body_changes, experience_text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(child.find('id').text, substanceName, substanceId, child.find('list-number').text, child.find('title').text, child.find('author').text, child.find('substance-string').text, child.find('body-weight').text,child.find('gender').text,child.find('published-date').text,child.find('submitted-date').text,child.find('experience-year').text, child.find('intensity').text,child.find('primary-category-id').text,child.find('substance-id-list').text,child.find('category-id-list').text,child.find('published-rating').text,child.find('body-changes').text,child.find('experience-text').text))
                 except:
                     pass
-                try:
+                finally:
+                    conn.commit()
+                    print('Record uploaded!')
 
-                    for child in root.findall('experience'):
-                        cursor.execute("INSERT INTO erowid.experiences (id, principal_substance, principal_substance_id, list_number, title, author, substance_string, body_weight, gender, published_date, submitted_date, experience_year, intensity, primary_category_id, substance_id_list, category_id_list, published_rating, body_changes, experience_text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(child.find('id').text, substanceName, substanceId, child.find('list-number').text, child.find('title').text, child.find('author').text, child.find('substance-string').text, child.find('body-weight').text,child.find('gender').text,child.find('published-date').text,child.find('submitted-date').text,child.find('experience-year').text, child.find('intensity').text,child.find('primary-category-id').text,child.find('substance-id-list').text,child.find('category-id-list').text,child.find('published-rating').text,child.find('body-changes').text,child.find('experience-text').text))
-                        conn.commit()
-                        print('Record uploaded!')
 
-                        experienceAlreadyCaptured.append(child.find('id').text)
-                except:
-                    pass
+                experienceAlreadyCaptured.append(child.find('id').text)
+
 
 
 cursor.close()
--- sentiment-evaluation.py
+++ sentiment-evaluation.py
@@ -12,12 +12,12 @@ rows = cursor.fetchall()
 
 for row in rows:
     id = row[0]
-    text = TextBlob(row[1])
-    sentimentPolarity = text.sentiment.polarity
-    sentimentSubjectivity = text.sentiment.subjectivity
+        text = TextBlob(row[1])
+        sentimentPolarity = text.sentiment.polarity
+        sentimentSubjectivity = text.sentiment.subjectivity
 
-    cursor.execute(
-        """UPDATE erowid.experiences SET sentiment_polarity = %s, sentiment_subjectivity = %s WHERE id = %s""",
+        cursor.execute(
+            """UPDATE erowid.experiences SET sentiment_polarity = %s, sentiment_subjectivity = %s WHERE id = %s""",
                                     (sentimentPolarity, sentimentSubjectivity, id))
 
 conn.commit()
